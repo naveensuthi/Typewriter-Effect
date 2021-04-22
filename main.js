b218e2ed6264b82342e3e3c423b1cb4f91ca6d7b
= <=>
2021-04-22 16:37:00 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,24 @@
+let text = document.getElementById("Typewriter");
+
+const textArray = text.textContent.split("");
+
+console.log(textArray);
+
+let timeLooper;
+
+text.innerHTML = "";
+console.log(text);
+
+function typewriterEffect() {
+  if (textArray.length > 0) {
+    text.innerHTML += textArray.shift();
+  } else {
+    clearTimeout();
+  }
+  timeLooper = setTimeout(
+    "typewriterEffect()",
+    Math.floor(Math.random() * (200, 250))
+  );
+}
+
+typewriterEffect();
